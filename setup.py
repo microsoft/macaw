950940a5424170ba4b2fbbec0d6a100d3fec9d51
Hamed Zamani <hazamani@microsoft.com>
2019-12-11 17:05:16 -0800

    create requirements for installation

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     license='MIT',
     author='Hamed Zamani',
     author_email='hazamani@microsoft.com',
-    description='An extensible conversational information seeking system'
+    description='An extensible framework for conversational information seeking research'
 )
